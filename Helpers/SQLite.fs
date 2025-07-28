eb6fde4ec630a3ba1c7a584857ca6ed6a486277e
MURRAY\vstarostin <defgoodone@gmail.com>
2025-07-28 18:52:39 +0300

    Introduce new .Helpers.SQLite module

---
+++ Helpers/SQLite.fs
@@ -0,0 +1,10 @@
+﻿module HabitsTracker.Helpers.Tests.SQLite
+
+open System.Data
+
+open Microsoft.Data.Sqlite
+
+let createInMemoryConnection () =
+    let conn = new SqliteConnection("Data Source=:memory:")
+    conn.Open()
+    conn :> IDbConnection
