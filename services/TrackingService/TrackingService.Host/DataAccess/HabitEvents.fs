b2e6a6bf35efdd689698ca95a024ac83465212b0
MURRAY\vstarostin <defgoodone@gmail.com>
2025-07-28 20:09:57 +0300

    Introduce DataAccess for HabitEvents table

---
+++ services/TrackingService/TrackingService.Host/DataAccess/HabitEvents.fs
@@ -0,0 +1,67 @@
+﻿module TrackingService.Host.DataAccess.HabitEvents
+
+open System
+open System.Data
+
+open Dapper
+open Dapper.FSharp.SQLite
+
+type HabitEvent =
+    { Id: int
+      HabitId: int
+      UserId: int
+      Date: DateTimeOffset }
+
+type InsertDto =
+    { HabitId: int
+      UserId: int
+      Date: DateTimeOffset }
+
+let habitEvents = table'<HabitEvent> "HabitEvents"
+
+let getAllAsync (conn: IDbConnection) =
+    select {
+        for h in habitEvents do
+            selectAll
+    }
+    |> conn.SelectAsync<HabitEvent>
+
+let getByIdsAsync (ids: int list) (conn: IDbConnection) =
+    select {
+        for h in habitEvents do
+            where (isIn h.Id ids)
+            selectAll
+    }
+    |> conn.SelectAsync<HabitEvent>
+
+let insertSingleAsync (insertDto: InsertDto) (conn: IDbConnection) =
+    task {
+        use tx = conn.BeginTransaction()
+
+        let! _ =
+            insert {
+                into (table'<InsertDto> "HabitEvents")
+                value insertDto
+            }
+            |> fun q -> conn.InsertAsync(q, tx)
+
+        let! id = conn.ExecuteScalarAsync<int>("SELECT last_insert_rowid()", transaction = tx)
+
+        tx.Commit()
+
+        return int id
+    }
+
+let deleteAllAsync (conn: IDbConnection) =
+    delete {
+        for _ in habitEvents do
+            deleteAll
+    }
+    |> conn.DeleteAsync
+
+let deleteByIdAsync (id: int) (conn: IDbConnection) =
+    delete {
+        for h in habitEvents do
+            where (h.Id = id)
+    }
+    |> conn.DeleteAsync
---
+++ services/TrackingService/TrackingService.Tests/HabitEvents.fs
@@ -0,0 +1,131 @@
+module TrackingService.Tests.DataAccess.HabitEvents
+
+open System
+
+open Microsoft.Data.Sqlite
+
+open NUnit.Framework
+open Dapper.FSharp.SQLite
+
+open HabitsTracker.Helpers
+open TrackingService.Host.DataAccess.HabitEvents
+open TrackingService.Migrations
+
+let connectionString = "Data Source=file:memdb1?mode=memory&cache=shared"
+
+[<OneTimeSetUp>]
+let Setup () =
+    do OptionTypes.register ()
+    do MigrationRunner.runMigrations connectionString (typeof<CreateTablesMigration>.Assembly)
+
+[<SetUp>]
+let clearDbAsync () =
+    task {
+        use conn = new SqliteConnection(connectionString)
+        let! _ = deleteAllAsync conn
+        return ()
+    }
+
+[<Test>]
+let ``Select from empty Habits returns empty`` () =
+    task {
+        use conn = new SqliteConnection(connectionString)
+        do conn.Open()
+        let! result = getAllAsync conn
+        Assert.That(result, Is.Empty)
+    }
+
+let date = DateTimeOffset.Parse "2025-07-28"
+
+[<Test>]
+let ``getByIdsAsync - ids selectivity`` () =
+    task {
+        use conn = new SqliteConnection(connectionString)
+        do conn.Open()
+        let! id1 = insertSingleAsync { HabitId = 1; Date = date } conn
+        let! id2 = insertSingleAsync { HabitId = 2; Date = date } conn
+        let! id3 = insertSingleAsync { HabitId = 3; Date = date } conn
+
+        let expectedInTable: HabitEvent list =
+            [ { Id = id1; HabitId = 1; Date = date }
+              { Id = id2; HabitId = 2; Date = date }
+              { Id = id3; HabitId = 3; Date = date } ]
+
+        let! actualInTable = getAllAsync conn
+        Assert.That(actualInTable, Is.EquivalentTo expectedInTable)
+
+        let expected: HabitEvent list = [ { Id = id2; HabitId = 2; Date = date }; { Id = id3; HabitId = 3; Date = date } ]
+        let! actual = getByIdsAsync [ id2; id3 ] conn
+        Assert.That(actual, Is.EquivalentTo expected)
+    }
+
+[<Test>]
+let ``getByIdsAsync - empty ids - no rows returned`` () =
+    task {
+        use conn = new SqliteConnection(connectionString)
+        do conn.Open()
+        let! id1 = insertSingleAsync { HabitId = 1; Date = date } conn
+        let! id2 = insertSingleAsync { HabitId = 2; Date = date } conn
+        let! id3 = insertSingleAsync { HabitId = 3; Date = date } conn
+
+        let expectedInTable: HabitEvent list =
+            [ { Id = id1; HabitId = 1; Date = date }
+              { Id = id2; HabitId = 2; Date = date }
+              { Id = id3; HabitId = 3; Date = date } ]
+
+        let! actualInTable = getAllAsync conn
+        Assert.That(actualInTable, Is.EquivalentTo expectedInTable)
+
+        let! actual = getByIdsAsync [] conn
+        Assert.That(actual, Is.Empty)
+    }
+
+[<Test>]
+let ``getByIdsAsync - unexistent id - no rows returned`` () =
+    task {
+        use conn = new SqliteConnection(connectionString)
+        do conn.Open()
+        let! id1 = insertSingleAsync { HabitId = 1; Date = date } conn
+        let! id2 = insertSingleAsync { HabitId = 2; Date = date } conn
+        let! id3 = insertSingleAsync { HabitId = 3; Date = date } conn
+
+        let expectedInTable: HabitEvent list =
+            [ { Id = id1; HabitId = 1; Date = date }
+              { Id = id2; HabitId = 2; Date = date }
+              { Id = id3; HabitId = 3; Date = date } ]
+
+        let! actualInTable = getAllAsync conn
+        Assert.That(actualInTable, Is.EquivalentTo expectedInTable)
+
+        let! actual = getByIdsAsync (List.singleton 0) conn
+        Assert.That(actual, Is.Empty)
+    }
+
+[<Test>]
+let ``getAllAsync - insert two then select all`` () =
+    task {
+        use conn = new SqliteConnection(connectionString)
+        do conn.Open()
+        let! id1 = insertSingleAsync { HabitId = 1; Date = date } conn
+        let! id2 = insertSingleAsync { HabitId = 2; Date = date } conn
+
+        let expected: HabitEvent list = [ { Id = id1; HabitId = 1; Date = date }; { Id = id2; HabitId = 2; Date = date } ]
+        let! actual = getAllAsync conn
+        Assert.That(actual, Is.EquivalentTo expected)
+    }
+
+[<Test>]
+let ``deleteByIdAsync - id selectivity`` () =
+    task {
+        use conn = new SqliteConnection(connectionString)
+        do conn.Open()
+        let! id1 = insertSingleAsync { HabitId = 1; Date = date } conn
+        let! id2 = insertSingleAsync { HabitId = 2; Date = date } conn
+
+        let! numRowsAffected = deleteByIdAsync id1 conn
+        Assert.That(numRowsAffected, Is.EqualTo 1)
+
+        let expected = List.singleton { Id = id2; HabitId = 2; Date = date }
+        let! actual = getAllAsync conn
+        Assert.That(actual, Is.EquivalentTo expected)
+    }
