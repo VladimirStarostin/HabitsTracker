451e7540edea078672ca661faf5d9a4700e76fe2
MURRAY\vstarostin <defgoodone@gmail.com>
2025-07-29 19:47:08 +0300

    Services: - Introduce connection string configuring - Introduce gRPC support

--- services/HabitsService/HabitsService.Host/Program.fs
+++ services/HabitsService/HabitsService.Host/Program.fs
@@ -1,18 +1,30 @@
-open System
 open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 
 open HabitsTracker.Helpers
 
+open HabitsService.Host.Grpc
+
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+
+    let connectionString = builder.Configuration.GetConnectionString("HabitsDb")
+    do builder.Services.AddGrpc()
+        .Services.AddScoped<HabitsServiceImpl>(
+            fun _ ->
+                new HabitsServiceImpl(connectionString)
+        ) |> ignore
+
     let app = builder.Build()
     do StatusEndpoint.add app |> ignore
+    do app.MapGrpcService<HabitsServiceImpl> () |> ignore
 
     do
         MigrationRunner.runMigrations
-            "Data Source=habits.db"
+            connectionString
             typeof<HabitsService.Migrations.CreateTablesMigration>.Assembly
 
     app.Run()
--- services/UserService/UserService.Host/Program.fs
+++ services/UserService/UserService.Host/Program.fs
@@ -1,15 +1,31 @@
-open System
 open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 
 open HabitsTracker.Helpers
 
+open UserService.Host.Grpc
+
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+
+    let connectionString = builder.Configuration.GetConnectionString("UserDb")
+    do builder.Services.AddGrpc()
+        .Services.AddScoped<UserServiceImpl>(
+            fun _ ->
+                new UserServiceImpl(connectionString)
+        ) |> ignore
+    
     let app = builder.Build()
     do StatusEndpoint.add app |> ignore
-    MigrationRunner.runMigrations "Data Source=users.db" typeof<UserService.Migrations.CreateTablesMigration>.Assembly
+    do app.MapGrpcService<UserServiceImpl> () |> ignore
+
+    do
+        MigrationRunner.runMigrations
+            connectionString
+            typeof<UserService.Migrations.CreateTablesMigration>.Assembly
 
     app.Run()
     0
