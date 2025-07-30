df230f77db65c1f34ae8fac7469ecfc699e54cc8
MURRAY\vstarostin <defgoodone@gmail.com>
2025-07-31 02:54:04 +0300

    UserService adjustments: - Introduce new RefreshRokens table migration - Rename existing migrations file and apply fantomas formatting

---
+++ services/UserService/UserService.Migrations/20250727001_CreateUsersTableMigration.fs
@@ -0,0 +1,17 @@
+﻿namespace UserService.Migrations
+
+open FluentMigrator
+
+[<Migration(20250727001L)>]
+type CreateTablesMigration() =
+    inherit Migration()
+
+    override _.Up() =
+        base.Create.Table("Users")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("Email").AsString(255).NotNullable().Unique()
+            .WithColumn("Name").AsString(512).NotNullable()
+            .WithColumn("Hash").AsString(200).NotNullable()
+        |> ignore
+
+    override _.Down() = base.Delete.Table("Users") |> ignore
---
+++ services/UserService/UserService.Migrations/20250730001_CreateRefreshTokensTableMigration.fs
@@ -0,0 +1,17 @@
+﻿namespace UserService.Migrations
+
+open FluentMigrator
+
+[<Migration(20250730001L)>]
+type CreateRefreshTokensTableMigration() =
+    inherit AutoReversingMigration()
+
+    override _.Up() =
+        base.Create
+            .Table("RefreshTokens").WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("Token").AsString(200).NotNullable().Unique()
+            .WithColumn("UserId").AsInt64().NotNullable()
+            .ForeignKey("FK_RefreshTokens_Users", "Users", "Id").WithColumn("ExpiresAt").AsDateTime().NotNullable()
+            .WithColumn("IsRevoked").AsBoolean().NotNullable().WithDefaultValue(false)
+            .WithColumn("CreatedAt").AsDateTime().NotNullable()
+        |> ignore
