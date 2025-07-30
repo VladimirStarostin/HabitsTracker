ee1b158939afb0e963c0a8b891e07aabe010ad88
MURRAY\vstarostin <defgoodone@gmail.com>
2025-07-31 02:52:15 +0300

    Infrastructure: Introduce script to start up all service projects (for testing purpose)

---
+++ services/up-all.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+#docker network create habitstracker-net
+
+for svc in pg_db HabitsService TrackingService UserService; do
+  echo "=== Starting $svc container ==="
+  (cd "$svc" && docker compose up --build --force-recreate -d)
+done
+
+echo "Done."
